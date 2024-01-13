4a4b5f3a2f574ff560ca9dfbdabc037c9358834f
Achilles <88937554+hulkhere@users.noreply.github.com>
2024-01-13 14:46:20 +0530

    code

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -7,43 +7,42 @@ import Background from "../public/assets/images/carousel/example.png";
 import ArrowLeft from "../public/assets/images/carousel/arrow-left.png";
 import ArrowRight from "../public/assets/images/carousel/arrow-right.png";
 
-const CarouselItem = ({ imageUrl, category, name, title, location, stars }) => {
-  return (
-    <div
-      style={{ backgroundImage: `url(${imageUrl})` }}
-      className="w-[23%] xl:w-[22%] h-[22rem] bg-cover rounded-2xl p-6"
-    >
-      <div className="flex h-full flex-col justify-between">
-        <div className="w-max bg-[#625DF5] px-4 py-2 gap-x-2 flex items-center rounded-full">
-          <div className="w-6">
-            <Image src={Cycling} alt="" />
-          </div>
-          <div className="text-white text-lg medium">{category}</div>
-        </div>
-        <div className="flex flex-col gap-y-3">
-          <div className="text-2xl bold text-white">{name}</div>
-          <div className="text-[#E6E6E6] text-lg medium">{title}</div>
-          <div className="flex justify-between items-center">
-            <div className="flex gap-x-1 items-center">
-              <div className="w-4">
-                <Image src={Location} alt="" />
-              </div>
-              <div className="text-[#E6E6E6] text-base medium">{location}</div>
-            </div>
-            <div className="flex gap-x-1 items-center">
-              <div className="w-4">
-                <Image src={Star} alt="" />
-              </div>
-              <div className="text-[#E6E6E6] text-base medium">{stars}</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
 const Carousel = () => {
+  const carouselItems = [
+    {
+      imageUrl: Background.src,
+      category: "Cycling",
+      name: "Shelby Martin",
+      title: "Indian Gold Medalist (2021)",
+      location: "Mumbai",
+      stars: "5 Stars (49)",
+    },
+    {
+      imageUrl: Background.src,
+      category: "Cycling",
+      name: "Shelby Martin",
+      title: "Indian Gold Medalist (2021)",
+      location: "Mumbai",
+      stars: "5 Stars (49)",
+    },
+    {
+      imageUrl: Background.src,
+      category: "Cycling",
+      name: "Shelby Martin",
+      title: "Indian Gold Medalist (2021)",
+      location: "Mumbai",
+      stars: "5 Stars (49)",
+    },
+    {
+      imageUrl: Background.src,
+      category: "Cycling",
+      name: "Shelby Martin",
+      title: "Indian Gold Medalist (2021)",
+      location: "Mumbai",
+      stars: "5 Stars (49)",
+    },
+  ];
+
   return (
     <div className="w-full flex justify-center">
       <div className="w-full flex flex-col gap-y-12 max-w-4xl xl:max-w-7xl py-16">
@@ -61,38 +60,48 @@ const Carousel = () => {
               <Image className="button-bg" src={ArrowLeft} alt="" />
             </div>
           </button>
-          <CarouselItem
-            imageUrl={Background.src}
-            category="Cycling"
-            name="Shelby Martin"
-            title="Indian Gold Medalist (2021)"
-            location="Mumbai"
-            stars="5 Stars (49)"
-          />
-          <CarouselItem
-            imageUrl={Background.src}
-            category="Cycling"
-            name="Shelby Martin"
-            title="Indian Gold Medalist (2021)"
-            location="Mumbai"
-            stars="5 Stars (49)"
-          />
-          <CarouselItem
-            imageUrl={Background.src}
-            category="Cycling"
-            name="Shelby Martin"
-            title="Indian Gold Medalist (2021)"
-            location="Mumbai"
-            stars="5 Stars (49)"
-          />
-          <CarouselItem
-            imageUrl={Background.src}
-            category="Cycling"
-            name="Shelby Martin"
-            title="Indian Gold Medalist (2021)"
-            location="Mumbai"
-            stars="5 Stars (49)"
-          />
+          {carouselItems.map((item, index) => (
+            <div
+              key={index}
+              style={{ backgroundImage: `url(${item.imageUrl})` }}
+              className="w-[23%] xl:w-[22%] h-[22rem] bg-cover rounded-2xl p-6"
+            >
+              <div className="flex h-full flex-col justify-between">
+                <div className="w-max bg-[#625DF5] px-4 py-2 gap-x-2 flex items-center rounded-full">
+                  <div className="w-6">
+                    <Image src={Cycling} alt="" />
+                  </div>
+                  <div className="text-white text-lg medium">
+                    {item.category}
+                  </div>
+                </div>
+                <div className="flex flex-col gap-y-3">
+                  <div className="text-2xl bold text-white">{item.name}</div>
+                  <div className="text-[#E6E6E6] text-lg medium">
+                    {item.title}
+                  </div>
+                  <div className="flex justify-between items-center">
+                    <div className="flex gap-x-1 items-center">
+                      <div className="w-4">
+                        <Image src={Location} alt="" />
+                      </div>
+                      <div className="text-[#E6E6E6] text-base medium">
+                        {item.location}
+                      </div>
+                    </div>
+                    <div className="flex gap-x-1 items-center">
+                      <div className="w-4">
+                        <Image src={Star} alt="" />
+                      </div>
+                      <div className="text-[#E6E6E6] text-base medium">
+                        {item.stars}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
           <button>
             <div className="w-12 absolute top-36 right-1 rounded-full border-2 border-[#625df5] inline-flex">
               <Image className="button-bg" src={ArrowRight} alt="" />
