1c22f7673fd8c440468f10125c957db5db0fa2fc
Achilles <88937554+hulkhere@users.noreply.github.com>
2024-01-24 18:14:11 +0530

    mobile-cards-updated

--- components/CarouselMob.jsx
+++ components/CarouselMob.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import Image from "next/image";
 import ExpertOneM from "../public/assets/images/carousel/expert-mob1.png";
 import ExpertTwoM from "../public/assets/images/carousel/expert-mob2.png";
@@ -9,7 +9,26 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const CustomSlider = ({ children }) => {
-  let slider;
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const handleBeforeChange = useCallback((current, next) => {
+    setCurrentSlide(next);
+  }, []);
+
+  const customPaging = (i) => (
+    <div
+      style={{
+        width: "10px",
+        height: "10px",
+        margin: "20px 0px",
+        listStyle: "none",
+        backgroundColor: i === currentSlide ? "#625DF5" : "white",
+        border: `2px solid #625DF5`,
+        borderRadius: "50%",
+        cursor: "pointer",
+      }}
+    />
+  );
 
   const settings = {
     infinite: true,
@@ -18,39 +37,17 @@ const CustomSlider = ({ children }) => {
     centerMode: true,
     centerPadding: "10%",
     arrows: false,
+    dots: true,
+    beforeChange: handleBeforeChange,
+    customPaging,
   };
-
-  // const LeftArrow = (props) => {
-  //   const { onClick } = props;
-  //   return <LeftButtonMob onClick={onClick} />;
-  // };
-
-  // const RightArrow = (props) => {
-  //   const { onClick } = props;
-  //   return <RightButtonMob onClick={onClick} />;
-  // };
-
-  // const goToNextSlide = () => {
-  //   if (slider) {
-  //     slider.slickNext();
-  //   }
-  // };
-
-  // const goToPrevSlide = () => {
-  //   if (slider) {
-  //     slider.slickPrev();
-  //   }
-  // };
 
   return (
     <div>
-      <Slider ref={(c) => (slider = c)} {...settings}>
-        {children}
-      </Slider>
+      <Slider {...settings}>{children}</Slider>
     </div>
   );
 };
-
 export default function CarouselMob() {
   return (
     <div>
