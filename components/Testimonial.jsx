e67674d3c5c53e96fd4fe95daafe09055031ceb9
togethrstore <developer@togethr.store>
2024-02-21 07:03:39 +0530

    vercel analytics added

--- components/Testimonial.jsx
+++ components/Testimonial.jsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState } from "react";
 import { Rating, LeftButton, RightButton, RatingMob } from "./Buttons";
 import { motion, AnimatePresence } from "framer-motion";
