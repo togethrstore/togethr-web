38381a90a1cea83d9f7bc87665f3b8cf45795820
togethrstore <developer@togethr.store>
2024-03-01 03:29:41 +0530

    header logo alignment fix

--- components/Headerr.jsx
+++ components/Headerr.jsx
@@ -21,9 +21,9 @@ export default function Headerr() {
   return (
     <>
       <div className="w-full flex justify-center items-center pt-8 px-6 lg:px-0 ">
-        <div className="w-full flex items-center justify-between gap-y-12 max-w-4xl xl:max-w-7xl">
+        <div className="w-full flex items-center justify-between gap-y-12 max-w-4xl xl:max-w-7xl mx-8">
 
-          <Link href={"/"} className="mx-6">
+          <Link href={"/"} >
             <div className="w-24 lg:w-32 cursor-pointer">
               <Image src={Logo} alt="" priority={true} />
             </div>
